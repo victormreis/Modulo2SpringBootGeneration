41aa1a730eb7fba0e6f75bc1b4fe382943843d99
Victor Martins <vitor_jhoul@hotmail.com>
2022-03-23 11:25:02 -0300

    Refatoração

--- introducao/src/main/java/com/desafioHelloWorld/desafio/controller/HelloController.java
+++ introducao/src/main/java/com/desafioHelloWorld/desafio/controller/HelloController.java
@@ -5,12 +5,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
 public class HelloController {
 	
-	@GetMapping
+	@RequestMapping("/hello")
 	public String hello() {
 		return "Hello Generation!";
+	}
+	
+	@GetMapping ("/bsm")
+	public String bsm () {
+		
+		return "Mentalidades Generation Brasil:\n1-Orientação ao futuro\n2-Responsabilidade Pessoal\n3-mentalidade de crescimento\n4-Persistencia"
+				+ "\nHabilidades:\n1-Trabalho em equipe\n2-Atençao aos detalhes\n3-Proatividade\n4-Comunicação";
+				
+	}
+	
+	@GetMapping ("/objetivos")
+	public String teste() {
+		return "Objetivos da semana:\nMeu principal objetivo está semana é entender melhor como funciona o Spring e conseguir aplicar todos os conceitos vistos no bloco 1 de forma eficiente!";
 	}
 
 }
